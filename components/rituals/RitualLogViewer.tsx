2b09c33b6fe1600d474328370c09abf7585ca373
SeriAce <seriace@hotmail.de>
2025-03-29 07:06:13 -0400

    🧿 Add all changes

--- components/rituals/RitualLogViewer.tsx
+++ components/rituals/RitualLogViewer.tsx
@@ -1,21 +1,17 @@
 import React from "react";
 
-type Props = {
-  filePath: string;
-  content: string;
+type RitualLogViewerProps = {
+  path: string[];
 };
 
-export class RitualLogViewer extends React.Component<Props> {
-  render() {
-    const { filePath, content } = this.props;
+export const RitualLogViewer: React.FC<RitualLogViewerProps> = ({ path }) => {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "monospace" }}>
+      <h2>⛧ RitualLogViewer</h2>
+      <p>This ritual listens to the following glyphic path:</p>
+      <pre>{JSON.stringify(path, null, 2)}</pre>
+    </div>
+  );
+};
 
-    return (
-      <div className="p-4 rounded-xl border border-neutral-300 bg-black text-green-400 font-mono whitespace-pre-wrap">
-        <div className="text-xs text-neutral-400 mb-2">
-          📜 Viewing scroll: <code>{filePath}</code>
-        </div>
-        <div>{content}</div>
-      </div>
-    );
-  }
-}
+export default RitualLogViewer;
