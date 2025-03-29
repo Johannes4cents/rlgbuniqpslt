01482a467a483c3cfce4c07efe0c9d03b0c55204
SeriAce <seriace@hotmail.de>
2025-03-29 05:06:56 -0400

    Add AphiScrollRouter placeholder

--- app/aphi/[...slug]/page.tsx
+++ app/aphi/[...slug]/page.tsx
@@ -1,10 +1,18 @@
 // app/aphi/[...slug]/page.tsx
 import React from "react";
 import { AphiScrollRouter } from "../../../components/aphi/AphiScrollRouter";
+import { RitualLogViewer } from "../../../components/ritual/RitualLogViewer"; // (create this soon!)
 
 export default class AphiPage extends React.Component<{ params: { slug?: string[] } }> {
   render() {
     const slug = this.props.params?.slug || [];
+
+    // 🌱 Here’s the conditional sprout:
+    if (slug[0] === "ritualLogs") {
+      return <RitualLogViewer path={slug.slice(1)} />;
+    }
+
+    // ✨ Default scroll path
     return <AphiScrollRouter slug={slug} />;
   }
 }
